97a34579b1b3d9fdb66d4d84c00cf71d6fd649e3
Sooryakrishna M <sk2004international@gmail.com>
2024-09-17 20:27:32 +0530

    Finalization

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -6,28 +6,33 @@ computer_win=0
 options = ["rock","paper","scissors"]
 
 while True:
-    user_choice=input("Enter rock/paper/scissors or q to quit the game")
+    user_choice=input("Enter rock/paper/scissors or q to quit the game ").lower()
     if user_choice == "q":
         break
     if user_choice not in options:
         continue
-    random=random.randint()
-    computer_choice=options[random]
+    random_number=random.randint(0,2)
+    computer_choice=options[random_number]
+    print("The computer picked ",computer_choice)
 
-    if user_choice== "rock" and computer_choice=="scissors":
+    if user_choice == "rock" and computer_choice=="scissors":
         print("You won!")
         user_win+=1
         continue
 
-    elif user_choice=="paper" and computer_choice=="rock":
+    elif user_choice =="paper" and computer_choice=="rock":
         print("You won!")
         user_win+=1
         continue
 
-    elif user_choice=="scissors" and computer_choice=="paper":
+    elif user_choice  =="scissors" and computer_choice=="paper":
         print("You won!")
         user_win+=1
         continue
+
+    elif user_choice==computer_choice:
+        print("It was a tie")
+
 
     else:
         print("You lose")
